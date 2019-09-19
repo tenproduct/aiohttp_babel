255a089db06d763a17e2bbe086c6f971d84feaaf
alexandrueneten <alexandruene@tengroup.com>
2019-09-19 14:08:31 +0100

    bump up version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 setup(
     name = "aiohttp-babel",
-    version = "0.0.7-ten",
+    version = "0.0.8-ten",
     packages = find_packages(),
 
     install_requires = [
